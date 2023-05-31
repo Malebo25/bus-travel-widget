7bb8d731d13927680c4a06e57599ed43ecd3e3f9
Malebo25 <malebomotlhake26@gmail.com>
2023-05-31 10:54:08 +0200

    pseudocode

---
+++ js/bus-travel.js
@@ -0,0 +1,9 @@
+// create a factory function called bus travel 
+
+// create a closures that set and return Points 
+// create variables that store number of single trips, price per trip  and initialize it to zero 
+// create variables that store number of return trips and price per return trip  
+
+// create a closure that takes in peak/off peak and location call it calculate 
+//  inside the calculate closure calculate and store number of single trips for peak or off-peak 
+// create a closure called return ; if user calls return calculate and store return values in return variable 
